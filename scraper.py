db41b905d9e36c0942d968d2908843607d7e9731
larawhyte <larawhyte@gmail.com>
2016-01-20 16:18:24 +0000

    Added simple print command

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@
 # import scraperwiki
 # import lxml.html
 #
-# # Read in a page
+print 'hello world'
 # html = scraperwiki.scrape("http://foo.com")
 #
 # # Find something on the page using css selectors
